4bd27126582573b6bfb2e71e2cb50ea26b79cb27
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-10-08 14:18:32 +0000

    Update ex_com.php

--- prices/ex_com.php
+++ prices/ex_com.php
@@ -4,6 +4,6 @@ $get = file_get_contents('https://api.dopehosting.net/namecheap/com.json');
 $json = json_decode($get, true);
 
 $dc = $json['CommandResponse']['UserGetPricingResult']['ProductType']['ProductCategory']['Product']['Price']['0']['@attributes']['Price'];
-//echo $dc;
+// output just price ex 10.00
 print_r($dc);
 ?>
