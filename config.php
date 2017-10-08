5ef606e85bc74d90e1c27a7602ed12a6d65e6f3a
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-10-08 11:01:29 +0000

    Create config.php

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+$config = array();
+
+$config['sql'] = array(
+     'host' => '127.0.0.1',
+     'database' => 'whmcs',
+     'sql_user' => 'root',
+     'user_pw' => 'itspassword'
+);
+
+$config['js0n'] = array(
+     'dir' => '/hostdata/api.dopehosting.net/public_html/namecheap/',
+     'c0m' => 'com.json',
+     'n3t' => 'net.json',
+     'xyz' => 'xyz.json'
+);
+
+$config['me'] = array(
+     'username' => 'YourUserName',
+     'apik3y' => 'YourApiKey'
+);
